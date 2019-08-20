e4af3f8e34d5c4860447a35bf9a1ebec046bdd97
Malvin Hariyanto <42708035+malvinh1@users.noreply.github.com>
2019-08-20 15:07:37 +0700

    Setup Initial Files

---
+++ front-end/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>The entreprenreur app.tsx EDIT BY MALVIN</Text>
+      <Text>The ABCD app.tsx EDIT BY MALVIN</Text>
+      <Text>BISA NGEDIT MASTER BRANCH LOH</Text>
+      <Text>BISA NGEDIT MASTER BRANCH LOH</Text>
+      <Text>BISA NGEDIT MASTER BRANCH LOH</Text>
+      <Text>BISA NGEDIT MASTER BRANCH LOH</Text>
+      <Text>BISA NGEDIT MASTER BRANCH LOH</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
