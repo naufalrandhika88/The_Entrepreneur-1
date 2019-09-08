4fe3203b0b747db2e0defc15263d53f83d3fac33
Malvin Hariyanto <malvinhariyanto50@gmail.com>
2019-09-08 21:11:11 +0700

    resolve conflict 2

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-<<<<<<< HEAD
 import { SERVER_OK, PORT } from './constants';
 import apiRouter from './routers';
 
@@ -32,50 +31,4 @@ app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log(`App is listening on http://127.0.0.1:${PORT}`);
-=======
-let app = express();
-
-let db = {
-  tasks: [
-    {
-      id: 1,
-      name: 'masak nasi',
-    },
-    {
-      id: 2,
-      name: 'masak air',
-    },
-  ],
-};
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send('success');
-});
-
-app.get('/task/', (req, res) => {
-  res.json(db.tasks);
-});
-
-app.get('/task/:id', (req, res) => {
-  let id = req.params.id;
-  for (let i = 0; i < db.tasks.length; i++) {
-    if (db.tasks[i].id === Number(id)) {
-      res.json(db.tasks[i]);
-    }
-  }
-});
-
-app.post('/task', (req, res) => {
-  let body = req.body;
-
-  res.json(body);
-  db.tasks.push(body);
-});
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
->>>>>>> 763092422e4417be4c3a2ed42d9e30834ec78d3c
 });
