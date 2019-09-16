2bde4ee439d8aae5361876cab2ea8552e4ac7eb7
Malvin Hariyanto <malvinhariyanto50@gmail.com>
2019-09-16 21:19:49 +0700

    rename component authCard.tsx

--- front-end/src/scenes/SignInScene.tsx
+++ front-end/src/scenes/SignInScene.tsx
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import AuthCard from '../component/authCard';
+import AuthCard from '../component/AuthCard';
 import { LinearGradient } from 'expo-linear-gradient';
-import {KeyboardAvoidingView} from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
 
 type Props = {};
 type State = {};
-
+Z;
 export default class SignInScene extends Component<Props, State> {
   render() {
     return (
       <View style={styles.view}>
-        <KeyboardAvoidingView  style={styles.view} behavior="padding" enabled>
+        <KeyboardAvoidingView style={styles.view} behavior="padding" enabled>
           <LinearGradient
             style={styles.container}
             colors={['#454545', '#000000']}
             start={{ x: 0, y: 0 }}
-            end={{ x: 0, y: 1 }}>
-              <AuthCard mode="singin"></AuthCard>
-            </LinearGradient>
+            end={{ x: 0, y: 1 }}
+          >
+            <AuthCard mode="singin"></AuthCard>
+          </LinearGradient>
         </KeyboardAvoidingView>
       </View>
     );
@@ -26,13 +27,13 @@ export default class SignInScene extends Component<Props, State> {
 }
 
 const styles = StyleSheet.create({
-  view:{
+  view: {
     flex: 1,
   },
   container: {
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
-    paddingBottom: 16
+    paddingBottom: 16,
   },
 });
--- front-end/src/scenes/SignUpScene.tsx
+++ front-end/src/scenes/SignUpScene.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, KeyboardAvoidingView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import AuthCard from '../component/authCard';
+import AuthCard from '../component/AuthCard';
 
 type Props = {};
 type State = {};
@@ -10,28 +10,29 @@ export default class SignUpScene extends Component<Props, State> {
   render() {
     return (
       <View style={styles.view}>
-      <KeyboardAvoidingView  style={styles.view} behavior="padding" enabled>
-        <LinearGradient
-        style={styles.container}
-        colors={['#454545', '#000000']}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 0, y: 1 }}>
-          <AuthCard mode="signup"></AuthCard>
-        </LinearGradient>
-       </KeyboardAvoidingView>
+        <KeyboardAvoidingView style={styles.view} behavior="padding" enabled>
+          <LinearGradient
+            style={styles.container}
+            colors={['#454545', '#000000']}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+          >
+            <AuthCard mode="signup"></AuthCard>
+          </LinearGradient>
+        </KeyboardAvoidingView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  view:{
+  view: {
     flex: 1,
   },
   container: {
     flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
-    paddingBottom: 16
+    paddingBottom: 16,
   },
 });
