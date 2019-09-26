5378a75293c287d9826ad6e9c33ca626bc3941e2
naufalrandhika88 <45902571+naufalrandhika88@users.noreply.github.com>
2019-09-26 11:37:12 +0700

    avatar 2

--- front-end/src/core-ui/Avatar.tsx
+++ front-end/src/core-ui/Avatar.tsx
@@ -1,61 +1,31 @@
 import React from 'react';
-import {StyleSheet, View, StyleProp, ViewStyle} from 'react-native';
-import {Image} from 'react-native-elements';
-
-import Icon from './Icon';
-import {WHITE} from '../constants/color';
+import { StyleSheet, StyleProp, ViewStyle } from 'react-native';
+import { Image } from 'react-native-elements';
 
 type Props = {
   newAvatarStyle?: StyleProp<ViewStyle>;
   src?: string | null;
-  sizeAvatar?: 'small' | 'medium' | 'large';
-  typeLocation?: boolean;
-};
-
-let size = {
-  small: 14,
-  medium: 26,
-  large: 90,
+  sizeAvatar?: 'medium';
 };
 
 export default function Avatar(props: Props) {
-  let {src, sizeAvatar, typeLocation, newAvatarStyle} = props;
+  let { src, sizeAvatar, newAvatarStyle } = props;
 
   return src ? (
     <Image
-      source={{uri: src}}
+      source={{ uri: src }}
       style={styles[sizeAvatar || 'medium']}
       containerStyle={newAvatarStyle}
     />
   ) : (
-    <View style={[styles[sizeAvatar || 'medium'], newAvatarStyle]}>
-      <Icon
-        nameIcon={typeLocation ? 'location-pin' : 'user'}
-        typeIcon={typeLocation ? 'entypo' : 'antdesign'}
-        colorIcon={WHITE}
-        sizeIcon={size[sizeAvatar || 'medium']}
-      />
-    </View>
+    <Image source={require('./assets/images/avatar.png')} />
   );
 }
 
 const styles = StyleSheet.create({
-  small: {
-    width: 24,
-    height: 24,
-    borderRadius: 12,
-    backgroundColor: '#FFC12E',
-  },
   medium: {
-    width: 36,
-    height: 36,
+    width: 56,
+    height: 56,
     borderRadius: 18,
-    backgroundColor: '#FFC12E',
-  },
-  large: {
-    width: 96,
-    height: 96,
-    borderRadius: 48,
-    backgroundColor: '#FFC12E',
   },
 });
