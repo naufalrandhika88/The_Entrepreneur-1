9fbaef11af883c5baf36640d404c9a6e47236e69
Malvin Hariyanto <malvinhariyanto50@gmail.com>
2019-11-29 21:49:29 +0700

    Update db.ts

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -1,17 +1,19 @@
-import { Client } from 'pg';
+import { Client, Pool } from 'pg';
 
 import { DB_PORT, USER, HOST, DATABASE_NAME, PASSWORD } from './constants';
 
 let db: Promise<Client> | undefined;
 
+let localConfig = {
+  user: USER,
+  host: HOST,
+  database: DATABASE_NAME,
+  password: PASSWORD,
+  port: DB_PORT,
+};
+
 async function connect() {
-  let client = new Client({
-    user: USER,
-    host: HOST,
-    database: DATABASE_NAME,
-    password: PASSWORD,
-    port: DB_PORT,
-  });
+  let client = new Pool(localConfig);
   try {
     await client.connect();
     console.log('Database connected!');
